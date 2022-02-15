ed6d709185546def06d1eb7178299a3dce7e82ef
wangkaiwd <1162486176@qq.com>
2022-02-15 20:26:05 +0800

    feat: render component successfully

--- packages/components/icon/src/icon.vue
+++ packages/components/icon/src/icon.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="w-icon">
-
+    <i class="w-icon-i" :style="style">
+      <slot></slot>
+    </i>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from 'vue';
+import { computed, defineComponent } from 'vue';
 import { iconProps } from './prop';
 
 export default defineComponent({
   name: 'WIcon',
   props: iconProps,
-  setup () {
-    return {};
+  setup (props) {
+    const style = computed(() => {
+      const result: Partial<CSSStyleDeclaration> = {};
+      const { color, size } = props;
+      if (props.size) {result.fontSize = size + 'px';}
+      if (props.color) {result.color = color;}
+      return result;
+    });
+    return {
+      style
+    };
   },
 });
 </script>
--- play/App.vue
+++ play/App.vue
@@ -1,3 +1,6 @@
 <template>
   <h2>Play</h2>
+  <w-icon color="red" size="40">
+    Icon
+  </w-icon>
 </template>
